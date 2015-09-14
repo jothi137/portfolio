92b2aca2754e015cfba9ccd34e8bf434b372ff19
Jothi Venkataraman <jothi137@gmail.com>
2015-09-13 22:23:37 -0500

    Adding about me

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -2,15 +2,21 @@
  * @author Jothi
  */
 var portfolio = function() {
-	var me = "I'm Jothi ";
+	var me = "I'm Jothi Venkataraman. I am a Software Engineer by profession. I am an all-round web developer " +
+	"with good knowledge of front-end techniques. I love learning new techniques and I insist on code quality. " +
+	"I love spending time on fixing little details and optimizing web apps. Also I like working in a team. ";
 	function initialize () {
 		var today = new Date();
 		$("#today")[0].innerHTML = today.toDateString();
-		$("li#aboutMe").addClass("active");
+		aboutMe();
 	};
 	
 	function aboutMe () {
-		console.log("aboutMe");
+		$("li#aboutMe").addClass("active"); 
+		$("#aboutMeContents").removeClass("hide");
+		$("#aboutMeContents").addClass("show");
+		var content = $("#aboutMeContents .text")[0];
+		content.innerHTML = me;
 	};
 	return {
 		initialize: initialize,
