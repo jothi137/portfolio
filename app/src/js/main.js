cfee2eb51d6569a53bb39c60f886f683d10b913a
Jothi Venkataraman <jothi137@gmail.com>
2015-09-14 22:41:41 -0500

    Cosmetic bug fixes
    Changing the title text and image

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -2,16 +2,12 @@
  * @author Jothi
  */
 var portfolio = function() {
-	var me = "I'm Jothi Venkataraman. I am a Software Engineer by profession. I am an all-round web developer " +
+	var me = "I'm Jothirlatha Venkataraman. " +
+	"I am a Software Engineer by profession. I am an all-round web developer " +
 	"with good knowledge of front-end techniques. I love learning new techniques and I insist on code quality. " +
 	"I love spending time on fixing little details and optimizing web apps. Also I like working in a team. ";
-	var bachSchool = "Amrita University, India. \n 2006 - 2010.",
-		bachDegree = "Information Technology"
-	
 	
 	function initialize () {
-		var today = new Date();
-		$("#today")[0].innerHTML = today.toDateString();
 		aboutMe();
 	};
 	
